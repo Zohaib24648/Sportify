5777b1834d9dac48f6567d1e7d09ebcf379fa3ba
Zohaib Mughal <z.mughal.24648@khi.iba.edu.pk>
2024-11-29 11:13:54 +0500

    doing some testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,23 @@
+import { INestApplication } from "@nestjs/common";
 import { AppModule } from "../src/app.module";
 import { Test } from "@nestjs/testing";
 
 
 describe('App e2e', () => {
+  let app : INestApplication;
   beforeAll(
     async () => {
       const moduleRef = await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
-})
-  it.todo('should display welcome message');
-  it.todo('should display welcome message 2');
 
+      app = moduleRef.createNestApplication();
+
+      await app.init();
+    })
+  afterAll(async () => {
+    await app.close();
+    });
+    
+  it.todo('should display welcome message');
 } );

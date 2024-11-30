25529264cec1bc92137e1253be7af03d266b95fd
Zohaib Mughal <z.mughal.24648@khi.iba.edu.pk>
2024-11-30 18:54:49 +0500

    added some swagger things

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -5,11 +5,15 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { Roles } from 'src/auth/guard/roles.decorator';
 import { UpdateUserDto } from './dto/updateuser.dto';
+import { ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('admin')
 export class AdminController {
 constructor (private admin_service: AdminService) {}
 
+
+@ApiOperation({ summary: 'Create a new user' })
+@ApiBearerAuth()
 @Roles('admin')
 @UseGuards(AuthGuard('jwt'),RolesGuard)
 @Post('create_user')
@@ -17,6 +21,8 @@ createUser(@Body() dto: SignupDto) {
     return this.admin_service.createUser(dto);}
 
 
+    @ApiOperation({ summary: 'update an existing user' })
+@ApiBearerAuth()
     @Roles('admin')
     @UseGuards(AuthGuard('jwt'),RolesGuard)
     @Put('update_user/:id')
@@ -24,7 +30,8 @@ createUser(@Body() dto: SignupDto) {
         return this.admin_service.updateUser(id, dto);}
     
 
-
+        @ApiOperation({ summary: 'delete a  user' })
+        @ApiBearerAuth()
         @Roles('admin')
         @UseGuards(AuthGuard('jwt'),RolesGuard)
         @Delete('delete_user/:id')
@@ -32,6 +39,9 @@ createUser(@Body() dto: SignupDto) {
             return this.admin_service.deleteUser(id);}
         
 
+            
+            @ApiOperation({ summary: 'Retrieve a list of users' })
+            @ApiBearerAuth()
             @Roles('admin')
             @UseGuards(AuthGuard('jwt'),RolesGuard)
             @Get('get_users')
